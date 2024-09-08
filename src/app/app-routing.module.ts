484b509c05148bdd7b8ec2002d0e03392de7239b
citeFred <citefred@gmail.com>
2024-09-08 20:58:53 +0900

    변경 적용

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,33 +1,16 @@
 import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { ArticleListPage } from './article/article-list/article-list.page';
 import { ArticleDetailPage } from './article/article-detail/article-detail.page';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: '',
-    redirectTo: '',
-    pathMatch: 'full'
-  },
-  { 
-    path: 'article-list', component: ArticleListPage 
-  },
-  { 
-    path: 'article/:id', component: ArticleDetailPage 
-  },
-  { 
-    path: '', redirectTo: 'article-list', pathMatch: 'full' 
-  }
+  { path: 'article-list', component: ArticleListPage },
+  { path: 'article-detail/:id', component: ArticleDetailPage },
+  { path: '', redirectTo: 'article-list', pathMatch: 'full' }
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
