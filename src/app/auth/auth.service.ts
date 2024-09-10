17011c28f8d7ae6e8ef03deb93056664f51c84ec
citeFred <citefred@gmail.com>
2024-09-10 15:04:01 +0900

    회원가입 fetch를 통한 JSON데이터 요청

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -4,6 +4,32 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AuthService {
+  private apiUrl = 'http://localhost:3000/api/auth';
 
   constructor() { }
+
+  async signUp(signUpData: any): Promise<any> {
+    try {
+      const response = await fetch(`${this.apiUrl}/signup`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(signUpData),
+      });
+  
+      // 응답 상태 코드와 메시지 확인
+      if (!response.ok) {
+        const errorText = await response.text(); // 에러 메시지 읽기
+        console.error('Network response was not ok:', errorText);
+        throw new Error(`Network response was not ok: ${response.statusText}`);
+      }
+  
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Fetch error:', error);
+      throw error;
+    }
+  }  
 }
