3c19cadb71f408fd3854f2cd081503b3793b7bb0
citeFred <citefred@gmail.com>
2024-09-10 13:06:07 +0900

    회원가입 및 로그인 인증 관련 모듈 및 라우팅 기초 설정

---
+++ src/app/auth/auth-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SignInComponent } from './signin/signin.component';
+import { SignUpComponent } from './signup/signup.component';
+
+const routes: Routes = [  
+  {
+    path: '',
+    redirectTo: 'signin',
+    pathMatch: 'full'
+  },
+  {
+    path: 'signin', component: SignInComponent
+  },
+  {
+    path: 'signup', component: SignUpComponent
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AuthRoutingModule { }
---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+
+import { AuthRoutingModule } from './auth-routing.module';
+import { SignInComponent } from './signin/signin.component';
+import { SignUpComponent } from './signup/signup.component';
+
+@NgModule({
+  declarations: [SignInComponent, SignUpComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    AuthRoutingModule
+  ]
+})
+export class AuthModule { }
