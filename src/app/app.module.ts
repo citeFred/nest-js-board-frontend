4e6602aba507497fce76f2b319ab098e053176c3
citeFred <citefred@gmail.com>
2024-09-10 18:39:17 +0900

    HttpClient 사용을 위한 루트 모듈 의존성 주입

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy, RouterModule } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-// import { ArticleDetailPageModule } from './article/article-detail/article-detail.module';
 import { ArticleListPageModule } from './article/article-list/article-list.module';
 import { ArticleDetailPageModule } from './article/article-list/article-detail/article-detail.module';
 import { HomePageModule } from './home/home.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
@@ -20,7 +18,8 @@ import { HomePageModule } from './home/home.module';
     AppRoutingModule,
     ArticleDetailPageModule,
     ArticleListPageModule,
-    HomePageModule
+    HomePageModule,
+    HttpClientModule,
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
