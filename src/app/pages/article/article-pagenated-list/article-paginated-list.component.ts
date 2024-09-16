2c590b14a5dd90589bc89ad4eb3d6959e90e9a5b
citeFred <citefred@gmail.com>
2024-09-16 14:24:57 +0900

    feat : 게시글 페이징 목록 조회 객체 및 페이지 카운트 추출

---
+++ src/app/pages/article/article-pagenated-list/article-paginated-list.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ArticleService } from '../../../services/article/article.service';
+import { ArticleResponseData } from 'src/app/models/article/article-response-data.interface';
+
+@Component({
+  selector: 'app-article-paginated-list',
+  templateUrl: './article-paginated-list.component.html',
+  styleUrls: ['./article-paginated-list.component.scss'],
+})
+export class ArticlePaginatedListComponent implements OnInit {
+  articles: ArticleResponseData[] = [];
+  currentPage: number = 1;
+  limit: number = 10;
+  totalPages: number = 0;
+  totalCount: number = 0;
+
+  constructor(private articleService: ArticleService, private router: Router) {}
+
+  ngOnInit() {
+    this.loadPaginatedArticles();
+  }
+
+  loadPaginatedArticles() {
+    this.articleService.getPaginatedArticles(this.currentPage, this.limit).subscribe({
+      next: response => {
+        if (response.success) {
+          this.articles = response.data.articles;
+          this.totalCount = response.data.totalCount;
+          this.totalPages = Math.ceil(this.totalCount / this.limit);
+        } else {
+          console.error(response.message);
+        }
+      },
+      error: err => {
+        console.error('Error fetching paginated articles:', err);
+      },
+      complete: () => {
+        console.log('Fetching paginated articles request completed.');
+      }
+    });
+  }
+
+  viewArticle(id: number) {
+    this.router.navigate(['/articles/detail', id]);
+  }
+
+  changePage(page: number) {
+    this.currentPage = page;
+    this.loadPaginatedArticles();
+  }
+}
