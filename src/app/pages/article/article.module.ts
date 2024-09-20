a08654d6dce9996bba398bb5faf22161563d9792
citeFred <citefred@gmail.com>
2024-09-20 16:43:04 +0900

    feat : 게시글 수정 컴포넌트 의존성 주입 관계 설정

--- src/app/pages/article/article.module.ts
+++ src/app/pages/article/article.module.ts
@@ -7,6 +7,7 @@ import { ArticleRoutingModule } from './article-routing.module';
 import { ArticleDetailComponent } from './article-detail/article-detail.component';
 import { ArticleWriteComponent } from './article-write/article-write.component';
 import { ArticlePaginatedListComponent } from './article-pagenated-list/article-paginated-list.component';
+import { ArticleUpdateComponent } from './article-update/article-update.component';
 
 @NgModule({
   imports: [
@@ -15,6 +16,6 @@ import { ArticlePaginatedListComponent } from './article-pagenated-list/article-
     IonicModule,
     ArticleRoutingModule
   ],
-  declarations: [ArticleListComponent, ArticlePaginatedListComponent, ArticleDetailComponent, ArticleWriteComponent]
+  declarations: [ArticleListComponent, ArticlePaginatedListComponent, ArticleDetailComponent, ArticleWriteComponent, ArticleUpdateComponent]
 })
 export class ArticleModule {}
