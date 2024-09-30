10e301b506adfd820d159d42134e7a095d94ea38
citeFred <citefred@gmail.com>
2024-09-30 19:39:41 +0900

    feat : 마이페이지 조회,수정,삭제 기능에 기본 AuthGuard 적용(루트라우팅모듈 전역)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {
-    path: '', component: HomeComponent
+    path: '', component: HomeComponent,
   },
   {
     path: 'articles',
@@ -16,7 +17,8 @@ const routes: Routes = [
   },
   {
     path: 'my-page',
-    loadChildren: () => import('./pages/user/user.module').then(m => m.UserModule)
+    loadChildren: () => import('./pages/user/user.module').then(m => m.UserModule),
+    canActivate: [AuthGuard]
   }
 ];
 
